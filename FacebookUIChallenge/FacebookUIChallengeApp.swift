19bcc21f4bf215d1b0c889aadac1fa8c3f32bc6b
Thai Duy Roan <128533558+tdr8012@users.noreply.github.com>
2025-02-06 12:57:49 -0500

    Initial Commit

---
+++ FacebookUIChallenge/FacebookUIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookUIChallengeApp.swift
+//  FacebookUIChallenge
+//
+//  Created by Roan Thai Quynh Vy on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FacebookUIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
